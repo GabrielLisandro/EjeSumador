813004b397a6ea0a224792737752beb7b29fc697
GabrielLisandro <glk.05@outlook.com>
2023-08-10 16:30:57 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/mavenproject2/EjemploSumador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author glk05
+ */
+public class EjemploSumador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
